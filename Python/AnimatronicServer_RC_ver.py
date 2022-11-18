d6a42cee797749b750a9a2b79105650b5bb61afe
emori <57478311+emor1@users.noreply.github.com>
2022-11-19 01:27:05 +0900

    commit

--- Python/AnimatronicServer_RC_ver.py
+++ Python/AnimatronicServer_RC_ver.py
@@ -1,10 +1,10 @@
 import asyncio
-from asyncio.windows_events import NULL
+# from asyncio.windows_events import NULL
 # WebSocketモジュールを宣言する。
 import websockets
 # クライアント接続すると呼び出す。
 import json
-from Rcb4Lib.Rcb4BaseLib import Rcb4BaseLib
+# from Rcb4Lib.Rcb4BaseLib import Rcb4BaseLib
 from configparser import ConfigParser
 
 # IPアドレス取得用
@@ -13,7 +13,7 @@ import socket
 import serial
 
 
-Eye_rotations = [90,90,90,90]
+RC_ServoAngles = [90,90,90,90,110,70,180]
 bias_angle = [0,0]
 
 # Arduino
@@ -22,14 +22,25 @@ EyeBallBoard = serial.Serial("COM12", 115200,timeout=3)
 
 def mapping(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
 def serial_communication(data1, data2,data3,data4):
     EyeBallBoard.write(b'a')   #データ送信合図
     EyeBallBoard.write(bytes([data1]))
     EyeBallBoard.write(bytes([data2]))
     EyeBallBoard.write(bytes([data3]))
     EyeBallBoard.write(bytes([data4]))
+
     print(data1, data2,data3, data4)
+
+# def serial_communication(data1, data2,data3,data4,data5,data6,data7):
+#     EyeBallBoard.write(b'a')   #データ送信合図
+#     EyeBallBoard.write(bytes([data1]))
+#     EyeBallBoard.write(bytes([data2]))
+#     EyeBallBoard.write(bytes([data3]))
+#     EyeBallBoard.write(bytes([data4]))
+#     EyeBallBoard.write(bytes([data5]))
+#     EyeBallBoard.write(bytes([data6]))
+#     EyeBallBoard.write(bytes([data7]))
+#     print(data1, data2,data3, data4,data5,data6,data7)
 
 async def accept(websocket, path):
     EyeStateChanged = False
@@ -41,49 +52,38 @@ async def accept(websocket, path):
         print(data)
         data_json = json.loads(data)
         for face_data in data_json:
-            if face_data == "JawOpen":
-                jaw = data_json["JawOpen"]
-                # angle = 8000
-                # if jaw > 0.08:
-                #     angle = 7400
-                
-                jaw_angle = mapping(jaw, 0.5, 0.0, 0.0, 1500.0)
-                jaw_angle += 6500
-                print(int(jaw_angle))
-                rcb4.setSingleServo(2, 1, int(jaw_angle), 2)
-            if face_data == "EyeBlinkLeft":
-                blink_l = data_json["EyeBlinkLeft"]
-                blink_l_anlge = mapping(blink_l, 0.0, 0.9, 7500, 10000)
-                rcb4.setSingleServo(0, 1, int(blink_l_anlge), 2)
-                print(blink_l)
-            if face_data == "EyeBlinkRight":
-                blink_r = data_json["EyeBlinkRight"]
-                blink_r_anlge = mapping(blink_r, 0.0, 0.9, 7500, 4500)
-                rcb4.setSingleServo(1, 1, int(blink_r_anlge), 2)
-                print(blink_r)
+            # if face_data == "JawOpen":
+            #     jaw = data_json["JawOpen"]
+            #     jaw_angle = mapping(jaw, 0.0, 0.9, 180.0, 100.0)
+            #     RC_ServoAngles[6]=jaw_angle
+            #     EyeStateChanged=True
 
-            # if  face_data == "EyeLookOutRight":
-            #     eye_outside_R = data_json["EyeLookOutRight"]
-            #     if eye_outside_R > 0.5:
-            #         Eye_rotations[0] = 20
-            #         Eye_rotations[1] = 150
-            #     else:
-            #         Eye_rotations[0] = 90
-            #         Eye_rotations[1] = 90
-            #     EyeStateChanged = True
+
+            # if face_data == "EyeBlinkLeft":
+            #     blink_l = data_json["EyeBlinkLeft"]
+            #     # 角度まだ
+            #     blink_l_Angle = mapping(blink_l, 0.0, 0.9, 110.0, 180.0)
+            #     RC_ServoAngles[4]=blink_l_Angle
+            #     EyeStateChanged=True
+
+            # if face_data == "EyeBlinkRight":
+            #     blink_r = data_json["EyeBlinkRight"]
+            #     blink_r_Angle = mapping(blink_r, 0.0, 0.9, 70.0, 0.0)
+            #     RC_ServoAngles[5]=blink_r_Angle
+            #     EyeStateChanged=True
 
             # 右目の動きのどちらかを検知
             # 右目の外側の動きのデータがあり、かつ０以上
             if  face_data == "EyeLookOutRight" and data_json["EyeLookOutRight"]>0.0:
                 eye_outside_R = data_json["EyeLookOutRight"]
-                eye_outside_Rmapping = mapping(eye_outside_R, 0.0, 0.9, 90, 43)
-                Eye_rotations[1]=eye_outside_Rmapping
+                eye_outside_Rmapping = mapping(eye_outside_R, 0.0, 0.9, 90.0, 43.0)
+                RC_ServoAngles[1]=eye_outside_Rmapping
                 EyeStateChanged=True
             # 右目の内側の動きのデータがあり、かつ０以上
             if face_data == "EyeLookInRight" and data_json["EyeLookInRight"]>0.0:
                 eye_inside_R = data_json["EyeLookInRight"]
-                eye_inside_Rmapping = mapping(eye_inside_R, 0.0, 0.9, 90, 120)
-                Eye_rotations[1]=eye_inside_Rmapping
+                eye_inside_Rmapping = mapping(eye_inside_R, 0.0, 0.9, 90.0, 120.0)
+                RC_ServoAngles[1]=eye_inside_Rmapping
                 # データを送るフラグを立てる
                 # // フラグを立てるのは、左目の更新をしてから動かすため
                 EyeStateChanged=True
@@ -93,13 +93,13 @@ async def accept(websocket, path):
             if face_data == "EyeLookOutLeft" and data_json["EyeLookOutLeft"]>0.0:
                 eye_outside_L = data_json["EyeLookOutLeft"]
                 eye_outside_Lmapping = mapping(eye_outside_L, 0.0, 0.9,74.0, 120.0 )
-                Eye_rotations[0]=eye_outside_Lmapping
+                RC_ServoAngles[0]=eye_outside_Lmapping
                 EyeStateChanged=True
             # 右目の内側の動きのデータがあり、かつ０以上
             if face_data =="EyeLookInLeft"and data_json["EyeLookInLeft"]>0.0:
                 eye_outside_L = data_json["EyeLookInLeft"]
                 eye_outside_Lmapping = mapping(eye_outside_L, 0.0, 0.9,74.0, 20.0 )
-                Eye_rotations[0]=eye_outside_Lmapping
+                RC_ServoAngles[0]=eye_outside_Lmapping
                 # データを送るフラグを立てる
                 # // フラグを立てるのは、左目の更新をしてから動かすため
                 EyeStateChanged=True
@@ -109,13 +109,13 @@ async def accept(websocket, path):
             if face_data == "EyeLookUpLeft" and data_json["EyeLookUpLeft"]>0.0:
                 eye_outside_L = data_json["EyeLookUpLeft"]
                 eye_outside_Lmapping = mapping(eye_outside_L, 0.0, 0.9,90.0, 46.0 )
-                Eye_rotations[2]=eye_outside_Lmapping
+                RC_ServoAngles[2]=eye_outside_Lmapping
                 EyeStateChanged=True
             # 左目の下の動きのデータがあり、かつ０以上
             if face_data =="EyeLookDownLeft"and data_json["EyeLookDownLeft"]>0.0:
                 eye_outside_L = data_json["EyeLookDownLeft"]
                 eye_outside_Lmapping = mapping(eye_outside_L, 0.0, 0.9, 90.0, 120.0)
-                Eye_rotations[2]=eye_outside_Lmapping
+                RC_ServoAngles[2]=eye_outside_Lmapping
                 # データを送るフラグを立てる
                 # // フラグを立てるのは、左目の更新をしてから動かすため
                 EyeStateChanged=True
@@ -125,13 +125,13 @@ async def accept(websocket, path):
             if face_data == "EyeLookUpRight" and data_json["EyeLookUpRight"]>0.0:
                 eye_outside_L = data_json["EyeLookUpRight"]
                 eye_outside_Lmapping = mapping(eye_outside_L, 0.0, 0.9,100.0, 120.0 )
-                Eye_rotations[3]=eye_outside_Lmapping
+                RC_ServoAngles[3]=eye_outside_Lmapping
                 EyeStateChanged=True
             # 右目の内側の動きのデータがあり、かつ０以上
             if face_data =="EyeLookDownRight"and data_json["EyeLookDownRight"]>0.0:
                 eye_outside_L = data_json["EyeLookDownRight"]
                 eye_outside_Lmapping = mapping(eye_outside_L, 0.0, 0.9,100.0, 73.0 )
-                Eye_rotations[3]=eye_outside_Lmapping
+                RC_ServoAngles[3]=eye_outside_Lmapping
                 # データを送るフラグを立てる
                 # // フラグを立てるのは、左目の更新をしてから動かすため
                 EyeStateChanged=True
@@ -139,7 +139,10 @@ async def accept(websocket, path):
             if EyeStateChanged:
                 EyeStateChanged = False
                 # シリアルでRCサーボの角度をマイコン経由で設定
-                serial_communication(int(Eye_rotations[0]),int(Eye_rotations[1]),int(Eye_rotations[2]),int(Eye_rotations[3]))
+                serial_communication(int(RC_ServoAngles[0]),int(RC_ServoAngles[1]),int(RC_ServoAngles[2]),int(RC_ServoAngles[3]))
+                # serial_communication(int(RC_ServoAngles[0]),int(RC_ServoAngles[1]),int(RC_ServoAngles[2]),int(RC_ServoAngles[3]),int(RC_ServoAngles[4]),int(RC_ServoAngles[5]),int(RC_ServoAngles[6]))
+
+
 
 
         # クライアントでechoを付けて再送信する。
@@ -147,8 +150,8 @@ async def accept(websocket, path):
 
 if __name__ == "__main__":
     print("Activate Server")
-    rcb4 = Rcb4BaseLib()
-    rcb4.open("COM11", 115200, 1.3)
+    # rcb4 = Rcb4BaseLib()
+    # rcb4.open("COM12", 115200, 1.3)
 
 
     print("Set Serial, and reading config file")
